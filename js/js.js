00b1ca7b669f89b0d6dbd5e6adaf1f57f6c5af18
MrRoboto <meorbass@gmail.com>
2024-01-22 20:04:40 +0100

    Efecto flip de la pagina de pelis.html en progreso

--- js/js.js
+++ js/js.js
@@ -1,21 +1,50 @@
 var flipPeliculas = document.querySelectorAll(".contenedorPeliculas");
+var flipOverlay = document.querySelectorAll(".overlay")
+var contenidoFlipped = document.querySelectorAll(".contenidoFlip")
+var contenidoDefault = document.querySelectorAll(".contenidoDefault")
 
-flipPeliculas.forEach(function(elemento) {
-    elemento.addEventListener('click', function() {
-        image.forEach(function(zoom) {
-            zoom.classList.add("backgroundZoom");
-            contenido.forEach(function(eliminar) {
-                eliminar.classList.add("eliminiarContenido");
-                contenedorTexto.forEach(function(textcontent) {
-                    textcontent.classList.add("mostarContenidoText")
-                    setTimeout(function() {
-                        index = 0;
-                        escribirTexto(nuevoTextoCompleto);
-                        Narrador.play();
-                        
-                    }, 3000);
-                });
+flipPeliculas.forEach(function (elemento) {
+    elemento.addEventListener('click', function () {
+        if (elemento.classList.contains("animacionDefault")) {
+            elemento.classList.remove("animacionDefault");
+            elemento.classList.add("animacionFlip");
+
+            flipOverlay.forEach(function (overlay) {
+                overlay.classList.remove("overlayDefault");
+                overlay.classList.add("overlayFlip");
             });
-        });
+
+            contenidoFlipped.forEach(function (contenido) {
+                contenido.classList.remove("contenidoFlip");
+                contenido.classList.add("contenidoFlipBlock");
+            });
+            contenidoDefault.forEach(function (cDefault){
+                
+                cDefault.classList.remove("contenidoDefault");
+                cDefault.classList.add("contenidoDefaultRemove");
+            });
+
+
+        } else {
+
+            elemento.classList.remove("animacionFlip");
+            elemento.classList.add("animacionDefault");
+
+            flipOverlay.forEach(function (overlay) {
+                overlay.classList.remove("overlayFlip");
+                overlay.classList.add("overlayDefault");
+            });
+
+            contenidoFlipped.forEach(function (contenido) {
+                contenido.classList.remove("contenidoFlipBlock");
+                contenido.classList.add("contenidoFlip");
+            });
+
+            contenidoDefault.forEach(function (cDefault){
+                
+                cDefault.classList.add("contenidoDefault");
+                cDefault.classList.remove("contenidoDefaultRemove");
+            });
+        }
     });
 });
