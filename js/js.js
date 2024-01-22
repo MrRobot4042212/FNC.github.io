7eaa0173f6384bbb565ca7213d3df1d2a0c68e25
Diego Alfonso Chicoma <meorbass@gmail.com>
2024-01-22 23:43:25 +0100

    Arreglando errores de Scripts en pelis.html

--- js/js.js
+++ js/js.js
@@ -2,49 +2,114 @@ var flipPeliculas = document.querySelectorAll(".contenedorPeliculas");
 var flipOverlay = document.querySelectorAll(".overlay")
 var contenidoFlipped = document.querySelectorAll(".contenidoFlip")
 var contenidoDefault = document.querySelectorAll(".contenidoDefault")
+var frontPeliculas = document.querySelectorAll(".peliculasActive");
+var backPeliculas = document.querySelectorAll(".peliculasBackInactive");
+var mostrarFiltros = document.querySelectorAll(".cuerpoPDPA");
+var botonFiltrar = document.querySelector(".filtros");
+
+
+
+
 
 flipPeliculas.forEach(function (elemento) {
     elemento.addEventListener('click', function () {
-        if (elemento.classList.contains("animacionDefault")) {
-            elemento.classList.remove("animacionDefault");
+        var overlay = elemento.querySelector(".overlay");
+        var contenidoFlipped = elemento.querySelector(".contenidoFlip");
+        var contenidoDefault = elemento.querySelector(".contenidoDefault");
+        var frontPelicula = elemento.querySelector(".peliculasActive");
+        var backPelicula = elemento.querySelector(".peliculasBackInactive");
+
+        if (!elemento.classList.contains("animacionFlip")) {
             elemento.classList.add("animacionFlip");
+            elemento.classList.remove("animacionDefault");
 
-            flipOverlay.forEach(function (overlay) {
-                overlay.classList.remove("overlayDefault");
-                overlay.classList.add("overlayFlip");
-            });
+            overlay.classList.remove("overlayDefault");
+            overlay.classList.add("overlayFlip");
 
-            contenidoFlipped.forEach(function (contenido) {
-                contenido.classList.remove("contenidoFlip");
-                contenido.classList.add("contenidoFlipBlock");
-            });
-            contenidoDefault.forEach(function (cDefault){
-                
-                cDefault.classList.remove("contenidoDefault");
-                cDefault.classList.add("contenidoDefaultRemove");
-            });
+            contenidoFlipped.classList.remove("contenidoFlip");
+            contenidoFlipped.classList.add("contenidoFlipBlock");
 
+            contenidoDefault.classList.remove("contenidoDefault");
+            contenidoDefault.classList.add("contenidoDefaultRemove");
 
-        } else {
+            frontPelicula.classList.remove("peliculasActive");
+            frontPelicula.classList.add("peliculasInactive");
 
+            backPelicula.classList.remove("peliculasBackInactive");
+            backPelicula.classList.add("peliculasBackActive");
+
+        } else if (overlay.classList.contains("overlayFlip")) {
             elemento.classList.remove("animacionFlip");
             elemento.classList.add("animacionDefault");
 
-            flipOverlay.forEach(function (overlay) {
-                overlay.classList.remove("overlayFlip");
-                overlay.classList.add("overlayDefault");
-            });
+            contenidoFlipped.classList.remove("contenidoFlipBlock");
+            contenidoFlipped.classList.add("contenidoFlip");
 
-            contenidoFlipped.forEach(function (contenido) {
-                contenido.classList.remove("contenidoFlipBlock");
-                contenido.classList.add("contenidoFlip");
-            });
+            contenidoDefault.classList.add("contenidoDefault");
+            contenidoDefault.classList.remove("contenidoDefaultRemove");
 
-            contenidoDefault.forEach(function (cDefault){
-                
-                cDefault.classList.add("contenidoDefault");
-                cDefault.classList.remove("contenidoDefaultRemove");
-            });
+            frontPelicula.classList.remove("peliculasInactive");
+            frontPelicula.classList.add("peliculasActive");
+
+            backPelicula.classList.remove("peliculasBackActive");
+            backPelicula.classList.add("peliculasBackInactive");
         }
     });
 });
+
+mostrarFiltros.forEach(function (showFilter) {
+    botonFiltrar.addEventListener('click', function () {
+        if(showFilter.style.gridTemplateColumns = "4fr"){
+            showFilter.style.gridTemplateColumns = "1fr 4fr";
+        }
+        else if(showFilter.style.gridTemplateColumns = "1fr 4fr"){
+            showFilter.style.gridTemplateColumns = "4fr";
+        }
+    });
+
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const ratingContainer = document.getElementById('rating');
+    const stars = ratingContainer.getElementsByClassName('star');
+  
+    for (let i = 0; i < stars.length; i++) {
+      stars[i].addEventListener('mouseover', function () {
+        highlightStars(i);
+      });
+  
+      ratingContainer.addEventListener('mouseleave', function () {
+        resetStars();
+      });
+  
+      stars[i].addEventListener('click', function () {
+        setRating(i + 1);
+      });
+    }
+  
+    function highlightStars(index) {
+      for (let i = 0; i <= index; i++) {
+        stars[i].classList.add('active');
+      }
+  
+      for (let i = index + 1; i < stars.length; i++) {
+        stars[i].classList.remove('active');
+      }
+    }
+  
+    function resetStars() {
+      for (let i = 0; i < stars.length; i++) {
+        stars[i].classList.remove('active');
+      }
+    }
+  
+    function setRating(rating) {
+      resetStars();
+  
+      for (let i = 0; i < rating; i++) {
+        stars[i].classList.add('active');
+      }
+    }
+  });
+  
