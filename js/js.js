e7453014c8810730f05471b286d3f700edce19c3
Diego Alfonso Chicoma <meorbass@gmail.com>
2024-01-25 02:24:47 +0100

    Arreglado efecto de "FLIP de contenedorPeliculas, añadido efectos para que sea mas intuitivo la informacion de las peliculas, añadido diversos scripts para los filtros.

--- js/js.js
+++ js/js.js
@@ -1,52 +1,81 @@
 var flipPeliculas = document.querySelectorAll(".contenedorPeliculas");
-
+var flipOverlay = document.querySelectorAll(".overlay")
+var contenidoFlipped = document.querySelectorAll(".contenidoFlip")
+var contenidoDefault = document.querySelectorAll(".contenidoDefault")
+var frontPeliculas = document.querySelectorAll(".peliculasActive");
+var backPeliculas = document.querySelectorAll(".peliculasBackInactive");
+var mostrarFiltros = document.querySelectorAll(".cuerpoPDPA");
+var botonFiltrar = document.querySelector(".masFiltros");
+var botonQuitarFiltros = document.querySelector(".menosFiltros");
+var advFilter = document.querySelector(".filtrosAvanzados");
+var basicFilter = document.querySelector(".filtrosBasicos");
 
 flipPeliculas.forEach(function (elemento) {
-    elemento.addEventListener('click', function cambiarCara () {
+  elemento.addEventListener('click', function () {
         if (elemento.classList.contains("animacionDefault")) {
-          
-
-
-        } else{
-          
-            elemento.classList.remove("animacionFlip");
-            elemento.classList.add("animacionDefault");
-
-            overlay.classList.add("overlayDefault");
-            overlay.classList.remove("overlayFlip");
-
-            contenidoFlipped.classList.remove("contenidoFlipBlock");
-            contenidoFlipped.classList.add("contenidoFlip");
-
-            contenidoDefault.classList.add("contenidoDefault");
-            contenidoDefault.classList.remove("contenidoDefaultRemove");
-
-            frontPelicula.classList.remove("peliculasInactive");
-            frontPelicula.classList.add("peliculasActive");
-
-            backPelicula.classList.remove("peliculasBackActive");
-            backPelicula.classList.add("peliculasBackInactive");
+            elemento.classList.remove("animacionDefault");
+            elemento.classList.add("animacionFlip");
         }
-    });
+        else if(elemento.classList.contains("animacionFlip")){
+          elemento.classList.remove("animacionFlip");
+          elemento.classList.add("animacionDefault");
+        }
+  });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  var botonDesglose = document.querySelectorAll('.botonGeneros');
+  var listaGeneros = document.querySelector('.listaGeneros');
+
+botonDesglose.forEach(function (botonDesglose) {
+    botonDesglose.addEventListener('click', function () {
+      if (listaGeneros.classList.contains('listaGenerosDesglose')) {
+        listaGeneros.classList.remove('listaGenerosDesglose');
+        listaGeneros.classList.add('listaGenerosEnglosar');
+      } else {
+        listaGeneros.classList.remove('listaGenerosEnglosar');
+        listaGeneros.classList.add('listaGenerosDesglose');
+      }
+    });
+  });
+});
+
+
 
 mostrarFiltros.forEach(function (showFilter) {
-    botonFiltrar.addEventListener('click', function () {
-        if(showFilter.style.gridTemplateColumns = "4fr"){
-            showFilter.style.gridTemplateColumns = "1fr 4fr";
-        }
-        else if(showFilter.style.gridTemplateColumns = "1fr 4fr"){
-            showFilter.style.gridTemplateColumns = "4fr";
-        }
-    });
+  botonFiltrar.addEventListener('click', function () {
+      if(showFilter.style.gridTemplateColumns = "4fr"){
+          showFilter.style.gridTemplateColumns = "1fr 4fr";
+          showFilter.style.alignItems = "start";
+          basicFilter.classList.add("filtrosBasicosRemove");
+          advFilter.classList.remove("filtrosAvanzadosRemove");
 
+
+          botonFiltrar.style.display="none";
+          botonQuitarFiltros.style.display="block";
+
+          botonQuitarFiltros.addEventListener('click', function () {
+            showFilter.style.gridTemplateColumns = "4fr";
+            botonFiltrar.style.display="block";
+            botonQuitarFiltros.style.display="none";
+            basicFilter.classList.remove("filtrosBasicosRemove");
+            advFilter.classList.add("filtrosAvanzadosRemove");
+          });
+      }
+      else if(showFilter.style.gridTemplateColumns = "1fr 4fr"){
+          showFilter.style.gridTemplateColumns = "4fr";
+      
+      }
+  });
 });
+
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    const ratingContainer = document.getElementById('rating');
-    const stars = ratingContainer.getElementsByClassName('star');
+    const ratingContainer = document.querySelector('.contenedorStars');
+    const stars = ratingContainer.querySelectorAll('.star');
   
+    stars.forEach(function (stars) {
     for (let i = 0; i < stars.length; i++) {
       stars[i].addEventListener('mouseover', function () {
         highlightStars(i);
@@ -85,4 +114,4 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     }
   });
-  
+})
