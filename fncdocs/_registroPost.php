4eaa583de31843dcd340d5582ada3763cbe0d563
MrRoboto <meorbass@gmail.com>
2024-03-05 23:18:28 +0100

    registro completado

--- fncdocs/_registroPost.php
+++ fncdocs/_registroPost.php
@@ -1,7 +1,6 @@
 <?php
 
-include("./initdb.php");
-
+require("./initdb.php");
 
 if (!isset($_POST['usuario'])){
     header("location: ./_registro.php");
@@ -13,12 +12,12 @@ if (!isset($_POST['nick'])){
     exit();
 }
 
-if (!isset($_POST['email']) || !preg_match('/^[a-zA-Z0-9][@]{1}[a-zA-Z0-9]', $_POST['email'])){
+if (!isset($_POST['email'])){
     header("location: ./_registro.php");
     exit();
 }
 
-if (!isset($_POST['password'])){
+if (!isset($_POST['contraseña'])){
     header("location: ./_registro.php");
     exit();
 }
@@ -28,23 +27,25 @@ $nick = $_POST['nick'];
 $email = $_POST['email'];
 $telefono = $_POST['telefono'];
 $contraseña = $_POST['contraseña'];
-$ccontraseña = $_POST['ccontraseña'];
 
-$consulta = mysqli_prepare($conn,
-    "INSERT INTO usuarios (usuario, nick, contraseña, email, telefono)
-    VALUES (?,?,?,?,?)
-");
+$consulta = mysqli_prepare($conn, "INSERT INTO usuarios(usuario, nick, contraseña, email, telefono) VALUES (?,?,?,?,?)");
+
+if ($consulta === false) {
+    die('Error preparing statement: ' . mysqli_error($conn));
+}
+
 mysqli_stmt_bind_param($consulta, "sssss", $usuario, $nick, $contraseña, $email, $telefono);
 
-mysqli_stmt_execute($consulta);
-
-if (mysqli_stmt_affected_rows($consulta) === 1){
-    header("location: ./_index.php");
-}
-else{
-    header("location: ./_registro.php");
+if (!mysqli_stmt_execute($consulta)) {
+    die('Error executing statement: ' . mysqli_stmt_error($consulta));
 }
 
-mysqli_close($conn);
+if (mysqli_stmt_affected_rows($consulta) === 1) {
+    echo "Registro exitoso!";
+} else {
+    echo "Error en el registro: " . mysqli_stmt_error($consulta);
+}
+
+mysqli_stmt_close($consulta);
 
 ?>
---
+++ fncdocs/registroTest.php
@@ -0,0 +1,8 @@
+<form action="./_registroPost.php" method="post">
+                <input name="usuario" type="text" placeholder="Usuario" required>
+                <input name="nick" type="text" placeholder="Nick" required>
+                <input name="email" type="text" placeholder="Correo electrónico" required>
+                <input name="telefono" type="text" placeholder="Teléfono" required>
+                <input name="contraseña" type="password" placeholder="Contraseña" required>
+                <button name="submit" type="submit" class="boton">Crear cuenta</button>
+ </form>
