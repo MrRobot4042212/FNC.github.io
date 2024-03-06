5a71876f81d5c5dbdfb5cc1ca49260587cd7d314
SirJousis <josiasgonzalez@outlook.com>
2024-03-06 20:10:29 +0100

    actoresPHP add auto

--- fncdocs/_actores.php
+++ fncdocs/_actores.php
@@ -1,18 +1,21 @@
+
+
+
 <?php
-    $titulo ="Actores";
-    $sloganPDPA="Actuaciones que inspiran, historias que perduran. Descubre el arte de la interpretación en nuestra página de películas sobre actores.";
+    $titulo = "Actores";
+    $sloganPDPA = "Actuaciones que inspiran, historias que perduran. Descubre el arte de la interpretación en nuestra página de películas sobre actores.";
 ?>
 
-<? require_once ("./_startPDPA.php")?>
+<?php require_once("./_startPDPA.php"); ?>
     <div class="cuerpoPDPA">
         <section class="filtros">
             <section class="filtrosBasicos">
-                <h5> Filtra por actor</h5>
+                <h5>Filtra por actor</h5>
                 <input type="text" name="nombreActor" class="inputActor" placeholder="Nombre del Actor">
-            </section>              
+            </section>
         </section>
         <section class="gridActor">
-            <? require ("./_contenedorActor.php")?>
+            <?php require_once("_contenedorActores.php"); ?>
         </section>
     </div>
-<? require_once ("./_endGeneral.php")?>
+<?php require_once("./_endGeneral.php"); ?>
---
+++ sql/insertActoresDelJosi.sql
@@ -0,0 +1,62 @@
+-- Joaquin Phoenix
+INSERT INTO actores(nombre, genero, premios, fechaNacimiento, nacionalidad, descripcion, srcImagen)
+VALUES (
+    "Joaquin Phoenix",
+    "Masculino",
+    "Premios de la Academia (Oscar): 1 premio y 4 nominaciones; Globos de Oro: 2 premios y 7 nominaciones; BAFTA: 1 premio y 4 nominaciones; Cannes Film Festival: 1 premio y 1 nominación",
+    "1974-10-28",
+    "estadounidense",
+    "Joaquin Phoenix es un talentoso actor estadounidense conocido por su versatilidad y profundidad en sus interpretaciones. Nacido el 28 de octubre de 1974 en Puerto Rico, Phoenix proviene de una familia de artistas y comenzó su carrera como actor infantil en la década de 1980. Sin embargo, fue en la década de 1990 cuando ganó reconocimiento por su actuación en películas como 'To Die For' (1995) y 'Quills' (2000).",
+    "/img/premios/MejorPremiosGlobos/phoenix.jpg"
+);
+
+-- Kevin Spacey
+INSERT INTO actores(nombre, genero, premios, fechaNacimiento, nacionalidad, descripcion, srcImagen)
+VALUES (
+    "Kevin Spacey",
+    "Masculino",
+    "Premios de la Academia (Oscar): 2 premios y 2 nominaciones; Globos de Oro: 1 premio y 8 nominaciones; BAFTA: 1 premio y 3 nominaciones; Premios Emmy: 6 nominaciones",
+    "1959-07-26",
+    "estadounidense",
+    "Kevin Spacey es un actor estadounidense conocido por su versatilidad y habilidad para interpretar una amplia gama de roles. Nacido el 26 de julio de 1959 en South Orange, Nueva Jersey, Spacey ha dejado una marca indeleble en el mundo del cine y el teatro. Ganador de múltiples premios, incluidos dos premios Oscar por sus actuaciones en 'The Usual Suspects' y 'American Beauty', Spacey es admirado por su talento para encarnar personajes complejos con profundidad y sutileza. Además de su trabajo en el cine, ha tenido una destacada carrera en el teatro, actuando en producciones de Broadway y siendo reconocido por su liderazgo como director artístico del prestigioso teatro londinense The Old Vic. Sin embargo, su carrera también ha sido marcada por controversias, incluidas acusaciones de conducta sexual inapropiada, que han impactado profundamente su reputación y carrera.",
+    "/img/premios/MejorPremiosGlobos/spacey.jpg"
+);
+
+-- Continúa con los inserts para los demás actores utilizando la misma estructura
+-- Liam Neeson
+INSERT INTO actores(nombre, genero, premios, fechaNacimiento, nacionalidad, descripcion, srcImagen)
+VALUES (
+    "Liam Neeson",
+    "Masculino",
+    "Premios de la Academia (Oscar): 1 nominación; Globos de Oro: 3 nominaciones; BAFTA: 1 nominación; Festival de Venecia: 1 premio y 1 nominación",
+    "1952-06-07",
+    "irlandés",
+    "Liam Neeson es un actor de cine y teatro de origen irlandés, nacido el 7 de junio de 1952 en Ballymena, Irlanda del Norte. Es conocido por su distintiva presencia en pantalla, su profunda voz y su capacidad para interpretar una amplia gama de roles, desde personajes de acción hasta dramas intensos.",
+    "/img/premios/MejorPremiosGlobos/neeson.jpg"
+);
+
+-- Morgan Freeman
+INSERT INTO actores(nombre, genero, premios, fechaNacimiento, nacionalidad, descripcion, srcImagen)
+VALUES (
+    "Morgan Freeman",
+    "Masculino",
+    "Premios de la Academia (Oscar): 1 premio y 5 nominaciones; Globos de Oro: 2 premios y 6 nominaciones; Festival de Berlín: 1 premio y 1 nominación; Premios Emmy: 1 nominación",
+    "1937-06-01",
+    "estadounidense",
+    "Morgan Freeman es un icónico actor estadounidense nacido el 1 de junio de 1937 en Memphis, Tennessee. Reconocido por su voz distintiva y su presencia magnética en pantalla, Freeman ha cautivado al público con su versatilidad y talento en una carrera que abarca décadas.",
+    "/img/premios/MejorPremiosGlobos/friman.jpg"
+);
+
+-- Robert De Niro
+INSERT INTO actores(nombre, genero, premios, fechaNacimiento, nacionalidad, descripcion, srcImagen)
+VALUES (
+    "Robert De Niro",
+    "Masculino",
+    "Premios de la Academia (Oscar): 2 premios y 8 nominaciones; Globos de Oro: 2 premios y 11 nominaciones; BAFTA: 7 nominaciones; Premios Emmy: 2 nominaciones",
+    "1943-08-17",
+    "estadounidense",
+    "Robert De Niro es un icónico actor estadounidense nacido el 17 de agosto de 1943 en Nueva York. Con una carrera que abarca más de cinco décadas, De Niro es reconocido por su versatilidad y habilidad para transformarse en una amplia gama de personajes.",
+    "/img/premios/MejorPremiosGlobos/deniro.jpg"
+);
+
+-- Continúa con los inserts para los demás actores utilizando la misma estructura
