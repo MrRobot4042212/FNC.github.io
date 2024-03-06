486ea02d2dcd8bc5be554464e4dc66fefbe75e5e
FelipeGM97 <felipegimeenzmartinez@gmail.com>
2024-03-06 15:59:05 +0100

    Cambio de la explicación del login

--- fncdocs/_loginPost.php
+++ fncdocs/_loginPost.php
@@ -1,22 +1,22 @@
 <?php
 require ("./initdb.php");
-// Obtener los valores del formulario de inicio de sesión
+// con esto se obtienen los valores del formulario de login
 $usuario = $_POST['usuario'];
 $contraseña = $_POST['contraseña'];
 
-// Consulta SQL para comprobar si el usuario y la contraseña existen en la base de datos
+// con esto se realiza una consulta mySQL par acomprobar si el nombre y la contraseña están en la base de datos
 $sql = "SELECT * FROM usuarios WHERE usuario='$usuario' AND contraseña='$contraseña'";
 $result = $conn->query($sql);
 
-// Comprobar si se encontró un usuario con las credenciales proporcionadas
+// Esto comprueba si se encontró un usuario con las credenciales proporcionadas
 if ($result->num_rows > 0) {
-    // Usuario autenticado correctamente, redireccionar a la página de inicio
+    // si se enceuntra, te lleva a la página de inicio
     header("Location: ./_index.php");
     exit();
 } else {
-    // Usuario o contraseña incorrectos, mostrar un mensaje de error
+    // si es incr¡orrecto, salta un mensaje de error
     echo "Usuario o contraseña incorrectos";
 }
-// Cerrar conexión a la base de datos
+// Cierra la conexión con la base de datos
 $conn->close();
 ?>
