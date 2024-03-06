560eb6232c09c1a6fc100266564eb38ab8839055
FelipeGM97 <felipegimeenzmartinez@gmail.com>
2024-03-06 16:01:52 +0100

    Correción de la ruta de redirección al index

--- fncdocs/_loginPost.php
+++ fncdocs/_loginPost.php
@@ -11,10 +11,10 @@ $result = $conn->query($sql);
 // Esto comprueba si se encontró un usuario con las credenciales proporcionadas
 if ($result->num_rows > 0) {
     // si se enceuntra, te lleva a la página de inicio
-    header("Location: ./_index.php");
+    header("Location: C:\FNC\index.html");
     exit();
 } else {
-    // si es incr¡orrecto, salta un mensaje de error
+    // si es incorrecto, salta un mensaje de error
     echo "Usuario o contraseña incorrectos";
 }
 // Cierra la conexión con la base de datos
