b3f6c4ddfd2c928d1495a1ae24d23431346efb6e
MrRoboto <meorbass@gmail.com>
2024-03-06 16:43:10 +0100

    insertActores fixed

--- sql/InsertsActores.sql
+++ sql/InsertsActores.sql
@@ -1,1 +1,17 @@
-SELECT * FROM fnc.actores;
+/* INSERTS PARA ACTORES */
+
+use fnc;
+
+INSERT INTO actores(nombre,genero,premios,fechaNacimiento,nacionalidad,descripcion,srcImagen)
+VALUES
+("Marlon Brando", "Masculino","Premios de la Academia (Oscar): 2 premios y 8 nominaciones", "1924-01-01", "estadounidense", "Marlon Brando fue un icónico actor estadounidense nacido el 3 de abril de 1924 en Omaha, Nebraska, y fallecido el 1 de julio de 2004 en Los Ángeles, California. Reconocido por su talento natural, carisma magnético y estilo único de actuación, Brando es considerado uno de los actores más influyentes en la historia del cine. Su carrera abarcó más de cinco décadas y dejó un legado imborrable en la industria cinematográfica. Brando se destacó por su capacidad para sumergirse en los personajes que interpretó, a menudo desafiando las convenciones establecidas y rompiendo moldes en su búsqueda de autenticidad y realismo.", "/img/premios/MejorPremiosGlobos/brandom.jpg");
+
+INSERT INTO actores(nombre,genero,premios,fechaNacimiento,nacionalidad,descripcion,srcImagen)
+VALUES
+("Al pacino", "Masculino","Globos de Oro: 5 premios y 20 nominaciones", "1940-02-02", "estadounidense", 'Al Pacino es un renombrado actor estadounidense nacido el 25 de abril de 1940 en Nueva York. Es reconocido por su talento versátil y su capacidad para interpretar una amplia gama de personajes con profundidad y autenticidad. Ganador de múltiples premios, incluyendo un Premio de la Academia por su actuación en "Scent of a Woman" (1992), Pacino ha dejado una huella indeleble en la industria del cine y el teatro. Su carrera abarca décadas y ha participado en icónicas películas como "The Godfather" (El Padrino), "Scarface", "Heat" y "Dog Day Afternoon". Además de su éxito en la pantalla grande, Pacino ha tenido una destacada trayectoria en el teatro, actuando en producciones tanto en Broadway como en el West End de Londres. Su carisma, intensidad y dedicación lo convierten en una figura legendaria en el mundo del entretenimiento.', "/img/premios/MejorPremiosGlobos/alpa.jpg");
+
+INSERT INTO actores(nombre,genero,premios,fechaNacimiento,nacionalidad,descripcion,srcImagen)
+VALUES
+("Robert Duvall", "Masculino","Premios de la Academia (Oscar): 1 premio y 7 nominaciones", "1931-03-03", "estadounidense", 'Robert Duvall es un actor estadounidense nacido el 5 de enero de 1931 en San Diego, California. Es reconocido por su versatilidad y habilidad para interpretar una amplia gama de personajes en cine y televisión. Duvall ha participado en numerosas películas aclamadas, incluyendo "El Padrino" (en la que interpretó a Tom Hagen), "Apocalypse Now", "Tender Mercies" (por la cual ganó el premio Oscar al Mejor Actor), "El Juez", entre otras. Su talento ha sido reconocido con múltiples premios y nominaciones a lo largo de su carrera, convirtiéndolo en una figura icónica de la actuación en Hollywood.
+', "/img/premios/MejorPremiosGlobos/duvall.jpg");
+
