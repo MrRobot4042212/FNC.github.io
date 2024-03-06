25d54fc60bf3ae582935856ad1d3e6cbdaf58bb1
FelipeGM97 <felipegimeenzmartinez@gmail.com>
2024-03-06 17:30:25 +0100

    Insert agregado a la tabla peliculas

---
+++ sql/InsertPeliculas.sql
@@ -0,0 +1,14 @@
+/* INSERTS PARA ACTORES */
+
+use fnc;
+
+INSERT INTO peliculas(nombre, genero, sinapsis, fechaEstreno, duracion, presupuesto, recaudacion, srcImagen)
+VALUES
+("Fight Club", "Thriller","El narrador (Edward Norton), que sufre de insomnio y cuyo nombre nunca se menciona,
+ es un empleado de una empresa aseguradora. Su médico se niega a recetarle medicación y, al quejarse de que está 
+ sufriendo, le replica que vaya a un grupo de apoyo para ver lo que es el sufrimiento de verdad. El narrador asiste
+ a un grupo de apoyo de víctimas de cáncer testicular y, después de convencerlos de que él también está aquejado de
+ la enfermedad, encuentra una liberación emocional que le cura el insomnio. Se convierte en un adicto a los grupos 
+ de terapia y a la farsa de hacerse pasar por víctima. Sin embargo, la presencia de Marla Singer (Helena Bonham Carter)
+ le molesta, ya que se da cuenta de que ella busca el mismo paliativo para los problemas en su vida y teme que lo delate
+ ante todos, por lo que negocia con ella para evitar encontrarse en las mismas reuniones.", "1999-09-10", 3,19, 63, "\img\peliculas\clublucha.jpg");
