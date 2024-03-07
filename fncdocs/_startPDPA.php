e88323aed8c13dc2ee504d1158f0c59fe7d020a8
SirJousis <josiasgonzalez@outlook.com>
2024-03-07 22:15:24 +0100

    hotFixesActores

--- fncdocs/_startPDPA.php
+++ fncdocs/_startPDPA.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$titulo?></title>
     <link rel="stylesheet" href="../css/styles.css">
-    <? session_start() ?>
 
 </head>
 <body >
